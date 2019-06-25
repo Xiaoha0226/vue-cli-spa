ca6bd9025362beba9de0f963c3d49802b7dd7431
苏晴 <suqing@rong360-MacBook82.local>
2019-06-26 00:08:10 +0800

    delete css config

--- dev/vue.config.js
+++ dev/vue.config.js
@@ -67,15 +67,5 @@ module.exports = {
       }),
       new vConsolePlugin({ enable: !!argv.debug }),
     ],
-  },
-  css: {
-    // 启用 CSS modules
-    modules: false,
-    // 是否使用css分离插件
-    extract: true,
-    // 开启 CSS source maps，一般不建议开启
-    sourceMap: false,
-    // css预设器配置项
-    loaderOptions: {},
-  },
+  }
 };
